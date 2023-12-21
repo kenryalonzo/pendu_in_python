e046d8cc980c6466210be43af3101d820696be96
GHOST <kenryalonzo@gmail.com>
2023-12-21 02:24:41 +0100

    fix: Hello everyone, today I present to you the V2 version of this game, in real life? I haven't changed much.

--- pendu.py
+++ pendu.py
@@ -1,3 +1,57 @@
+import random
+
+def generate_mystey_word(dictionnaire):
+    mot = random.choice(dictionnaire)
+    mot_len = len(mot)
+    mystere = ""
+    
+    for i in range(mot_len):
+        if i%2 == 0:
+            mystere += mot[i]
+        else:
+            mystere += "*"
+            
+    return mot, mystere 
+
+
+def check_it(mystere, mot, user_try):
+    status = False
+    msg = None
+    if not len(user_try) == len(mot):
+        msg = f"Desole vous devez entre un mot contenant {len(mot)} caractere!"
+        return status, msg, mystere
+    
+    if not user_try == mot:
+        mystere_backup = mystere
+        mystere = ""
+        for i in range(len(mot)):
+            if user_try[i] == mot[i]:
+                if i > 0:
+                    if mot[i-1] == user_try[i-1]:
+                        mystere += mot[i]
+                    else:
+                        mystere += mystere_backup[i]
+                else:
+                    mystere += mystere_backup[i]
+            msg = f"Desole le mot mystere entre ne correspond pas!"
+            return status, msg, mystere
+    
+    msg = f"Felicitation vous avez trouvez le mot cache : {mot}"
+    status = True
+    return status, msg, mystere
+
+
+def play(dictionnaire = ["test", "mystere"]):
+    mot, mystere = generate_mystey_word(dictionnaire)
+    test = False
+    
+    while not test:
+        print(mystere)
+        user_try = input("Quel est le mot mystere ? ")
+        test, msg, mystere = check_it(mystere, mot, user_try)
+        print(msg)
+  
+        
 dico = [
     "test",
     "vie",
@@ -7,40 +61,4 @@ dico = [
     "cacher"
 ]
 
-mot = dico[3]
-mot_len = len(mot)
-
-mystere = ""
-test = True
-
-for i in range(mot_len):
-    if i%2 == 0:
-        mystere += mot[i]
-    else:
-        mystere += "*"
-
-
-while test:
-
-    print(mystere)
-    mot_utilisateur = input("Quel est le mot mystere: ")
-
-    if not len(mot_utilisateur) == mot_len:
-        print(f"Desole vous devez entre un mot contenant {mot_len} caractere!")
-    else:
-        if not mot_utilisateur == mot:
-            mystere_backup = mystere
-            mystere = ""
-            for i in range(mot_len):
-                if mot_utilisateur[i] == mot[i]:
-                    if i > 0:
-                        if mot[i-1] == mot_utilisateur[i-1]:
-                            mystere += mot[i]
-                    else:
-                        mystere += mystere_backup[i]
-                else:
-                    mystere += mystere_backup[i]
-            print(f"Desole le mot mystere entre ne correspond pas!")
-        else:
-            print(f"Felicitation vous avez trouvez le mot cache : {mot}")
-            test = False
+play(dico)
